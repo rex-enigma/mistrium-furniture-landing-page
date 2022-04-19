fd0f3f29446cc46e4bbb37ec022b05537a8aeb94
rex-enigma <infinityjosh.jn@gmail.com>
2022-04-19 15:42:13 +0300

    .

--- src/data.js
+++ src/data.js
@@ -6,4 +6,53 @@ export const headerData = {
     subtitle:'In unfeeling existence objection immediate repulsive on he in. Imprudence comparison uncommonly me he difficulty diminution resolution. Likewise proposal differed scarcely dwellingxas on raillery.'
 };
 
- 
+export const furnitureData = [
+    {
+        title: 'Residential Furniture',
+        bgColor: '#FCEFE9',
+    },
+    {
+        title: 'Hospitality Furniture',
+        bgColor: '#FAE2C8'
+    }
+  ];
+
+
+export const productData = [
+    {
+        name: '',
+        currentPrice:'',
+        previousPrice: '',
+    
+    },
+    {
+        name: '',
+        currentPrice:'',
+        previousPrice: '',
+    
+    },
+    {
+        name: '',
+        currentPrice:'',
+        previousPrice: '',
+    
+    },
+    {
+        name: '',
+        currentPrice:'',
+        previousPrice: '',
+    
+    },
+    {
+        name: '',
+        currentPrice:'',
+        previousPrice: '',
+    
+    },
+    {
+        name: '',
+        currentPrice:'',
+        previousPrice: '',
+    
+    },
+];
